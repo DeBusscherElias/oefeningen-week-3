940253e8698edbd2f05721327b9da82ae54e3f9a
DeBusscherElias <elias.de.busscher@student.howest.be>
2020-10-06 14:20:48 +0200

    upload

--- oef-08.py
+++ oef-08.py
@@ -1,8 +1,14 @@
-start = 0
+# start = 0
+# stop = 100
+# som = 0
+
+# for getal in range(start, stop+1):
+#     som = som + getal
+
+# print(f'{som}')
+
 stop = 100
-som = 0
+getal = 1
 
-for getal in range(start, stop+1):
-    som = som + getal
-
-print(f'{som}')
+while getal <= stop:
+    print(getal)
